7d12063993479b5d9e1bac21c5fa6b906a6cdc78
Keren Nzira <kerennzira@gmail.com>
2024-08-28 00:05:44 +0100

    noBreak code

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-// var findWordsContaining = function(words, x) {
-//     let indices = words.map((word, index) => word.includes(x)?index:-1);
-    
-//     return indices.filter(index => index !== -1); 
-// };
-//la fonction ci dessus est celle avec la methode map mais je la trouve plus difficile à comprendre que celle que je vais mettre ci dessous
-
-//la function proprement dite
 var findWordsContaining = function(words, x) {
-        let indices = [];
-        words.forEach((word,i) => {
-            if(word.includes(x)){
-                indices.push(i);
-            }
-        })
-        return indices;
-    };
+    let indices = [];
+   for(let i = 0; i<words.length; i++){
+    let word = words[i];
+    if(word.includes(x)){
+        indices.push(i);
+    }
+   }
+   return indices; 
+};
